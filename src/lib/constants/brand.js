70cd473441858b262c80aca79e44adf23760a942
xiao-xiao-lang <xiao_xiao_lang@outlook.com>
2026-03-08 22:04:50 +0800

    修BUG：改网站标题。

--- src/lib/constants/brand.js
+++ src/lib/constants/brand.js
@@ -1,7 +1,7 @@
 // Legacy export format because this is used by some build-time scripts stuck in the past.
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    APP_NAME: 'Bilup',
-    FEEDBACK_URL: 'https://github.com/orgs/Bilup/discussions/new?category=bug-report-unsure-repo',
-    GITHUB_URL: 'https://github.com/Bilup'
+    APP_NAME: 'RemixWarp',
+    FEEDBACK_URL: 'https://github.com/orgs/RemixWarp/discussions/new?category=bug-report-unsure-repo',
+    GITHUB_URL: 'https://github.com/RemixWarp'
 };
