3e6067b44c11f494d9e01c84e7f4607c02005798
xiao-xiao-lang <xiao_xiao_lang@outlook.com>
2026-03-23 18:45:47 +0800

    __1.2.55__ -修复Sprite File List View 插件中文文本 -Sprite File List View 插件中文名为角色文件列表视图

--- src/addons/addons/sprite-folders/_manifest_entry.js
+++ src/addons/addons/sprite-folders/_manifest_entry.js
@@ -1,6 +1,11 @@
 const manifest = {
     name: 'Sprite File List View',
     description: 'Transforms the sprite list into a VSCode-style file explorer with folders (works with // folder naming).',
+    credits: [
+        {
+            name: '笑小朗（翻译）'
+        }
+    ],
     tags: ['editor', 'sprites', 'MistWarp'],
     userscripts: [
         {
