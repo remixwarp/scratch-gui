ba2cb49e6f8fbd2eb7581e41ac6f22528fbf3a4b
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-06-27 23:03:30 +0300

    Add files via upload

---
+++ src/lib/themes/accent/light-blue.js
@@ -0,0 +1,17 @@
+const guiColors = {
+    'motion-primary': 'hsla(194, 100%, 50%, 1)',
+    'motion-primary-transparent': 'hsla(203, 100%, 39%, 1)',
+    'motion-tertiary': 'hsla(203, 100%, 39%, 1)',
+    'motion-primary-dark': '#009CCC',
+
+    'extensions-primary': 'hsla(194, 100%, 50%, 1)',
+    'extensions-tertiary': 'hsla(203, 100%, 39%, 1)',
+    'extensions-transparent': 'hsla(194, 100%, 50%, 0.35)',
+    'extensions-light': 'hsla(203, 100%, 39%, 1)',
+    
+    'drop-highlight': 'hsla(203, 100%, 39%, 1)'
+};
+
+export {
+    guiColors as default
+};
---
+++ src/lib/themes/accent/lime.js
@@ -0,0 +1,17 @@
+const guiColors = {
+    'motion-primary': 'hsla(92, 91%, 53%, 1)',
+    'motion-primary-transparent': 'hsla(92, 92%, 53%, 0.35)',
+    'motion-tertiary': 'hsla(92, 80%, 43%, 1)',
+    'motion-primary-dark': 'hsla(84, 100%, 41%, 1)',
+
+    'extensions-primary': 'hsla(92, 91%, 53%, 1)',
+    'extensions-tertiary': 'hsla(92, 80%, 43%, 1)',
+    'extensions-transparent': 'hsla(92, 91%, 53%, 0.35)',
+    'extensions-light': 'hsla(92, 80%, 43%, 1)',
+
+    'drop-highlight': 'hsla(92, 91%, 59%, 1)'
+};
+
+export {
+    guiColors as default
+};
