43b233a77934eb877025df313e636602b10c62d4
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-07-01 14:04:29 +0300

    Update magenta.js

--- src/lib/themes/accent/magenta.js
+++ src/lib/themes/accent/magenta.js
@@ -1,7 +1,27 @@
 const guiColors = {
-    'motion-primary': 'hsla(289, 100%, 54%, 1)'
+    'motion-primary': '#FF269A',
+    'motion-primary-transparent': '#FF269Ae6',
+    'motion-tertiary': '#E60D8C',
+
+    'looks-secondary': '#FF269A',
+    'looks-transparent': '#FF4DC159',
+    'looks-light-transparent': '#FF4DC126',
+    'looks-secondary-dark': 'hsla(330, 68%, 57%, 1)',
+
+    'extensions-primary': 'hsla(315, 85%, 65%, 1)',
+    'extensions-tertiary': 'hsla(315, 85%, 57%, 1)',
+    'extensions-transparent': 'hsla(315, 85%, 65%, 0.43)',
+    'extensions-light': 'hsla(315, 57%, 85%, 1)',
+
+    'drop-highlight': '#FF8ADC'
+};
+
+const blockColors = {
+    checkboxActiveBackground: '#FF269A',
+    checkboxActiveBorder: '#FC00B9'
 };
 
 export {
-    guiColors as default
+    guiColors,
+    blockColors
 };
