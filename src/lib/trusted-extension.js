815ae7c8bce35250108051dec737473b5c307b5b
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-08-03 23:06:41 +0300

    Update trusted-extension.js

--- src/lib/trusted-extension.js
+++ src/lib/trusted-extension.js
@@ -1,6 +1,7 @@
 const isGalleryExtensionUrl = url => (
     url.startsWith('https://extensions.turbowarp.org/') ||
-    url.startsWith('https://extensions.mistium.com/')
+    url.startsWith('https://extensions.mistium.com/') ||
+    url.startsWith('https://extensions.bilup.org/')
 );
 
 const isTrustedExtensionUrl = url => isGalleryExtensionUrl(url) || url.startsWith('http://localhost:8000/');
