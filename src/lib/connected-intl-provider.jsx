809c4216864d16b759fd4358e4a436b803072636
xiao-xiao-lang <xiao_xiao_lang@outlook.com>
2026-08-07 16:41:52 +0800

    修复中文

--- src/lib/connected-intl-provider.jsx
+++ src/lib/connected-intl-provider.jsx
@@ -3,6 +3,8 @@ import {IntlProvider as ReactIntlProvider} from 'react-intl';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
+import IntlBridge from './tw-use-intl.jsx';
+
 class ConnectedIntlProvider extends React.Component {
     static propTypes = {
         messages: PropTypes.object
@@ -12,14 +14,20 @@ class ConnectedIntlProvider extends React.Component {
         if (prevProps.messages !== this.props.messages) {
             console.log('=== ConnectedIntlProvider messages updated ===');
             console.log('messages keys count:', Object.keys(this.props.messages || {}).length);
-            
+
             const sampleKeys = Object.keys(this.props.messages || {}).filter(k => k.startsWith('gui.menuBar')).slice(0, 3);
             console.log('sample menuBar translations:', sampleKeys.map(k => ({[k]: this.props.messages[k]})));
         }
     }
 
     render() {
-        return <ReactIntlProvider {...this.props} />;
+        return (
+            <ReactIntlProvider {...this.props}>
+                <IntlBridge>
+                    {this.props.children}
+                </IntlBridge>
+            </ReactIntlProvider>
+        );
     }
 }
 
