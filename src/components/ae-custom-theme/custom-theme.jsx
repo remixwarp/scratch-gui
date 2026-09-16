f872c1691d0ae0f239af42466b860e77fec542c8
xiao-xiao-lang <xiao_xiao_lang@outlook.com>
2026-09-16 11:33:11 +0000

    fix: close Box tag in custom-theme modal JSX

--- src/components/ae-custom-theme/custom-theme.jsx
+++ src/components/ae-custom-theme/custom-theme.jsx
@@ -243,7 +243,7 @@ const CustomModalComponent = props => {
                         />
                     </button>
                 </div>
-            </div>
+            </Box>
 
         </Modal>
     );
