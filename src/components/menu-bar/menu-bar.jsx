ef053663183cd0401affe611df6fe17991a3206a
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-09-20 17:08:38 +0300

    Update menu-bar.jsx

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -176,7 +176,7 @@ import {
     FilePlusCorner, Upload, RefreshCcw, ClockPlus, Package, FileInput,
     Save, ArchiveRestore, UserPen, Cloud, Settings, PackagePlus, Puzzle,
     Bookmark, GitBranch, FileCog, Bug, Database, Undo, Redo, Handshake, TerminalSquare, Sparkles, Wrench, Keyboard,
-    Zap, Gauge, BookOpen, Code, Trophy, ListTodo, Map, Activity, Store, Backpack, Terminal, ExternalLink
+    Zap, Gauge, BookOpen, Code, Trophy, ListTodo, SimpleAnalyzer, Map, Activity, Store, Backpack, Terminal, ExternalLink
  } from 'lucide-react';
 
 import sharedMessages from '../../lib/constants/shared-messages';
@@ -3801,7 +3801,7 @@ class MenuBar extends React.Component {
                                                this.props.onRequestCloseTools();
                                            }}
                                        >
-                                           <ChartColumn />
+                                           <SimpleAnalyzer />
                                            <FormattedMessage
                                                defaultMessage="Simple Project Analyzer"
                                                description="Menu bar item to toggle the simple project analyzer"
