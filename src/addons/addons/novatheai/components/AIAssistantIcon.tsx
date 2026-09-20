5c408a401cba3b4ca6a2ca6876f2fdc54c2fd632
ScratchCatStudios <romanarsenyk11@gmail.com>
2026-09-20 17:14:38 +0300

    Delete src/addons/addons/novatheai directory

--- src/addons/addons/novatheai/components/AIAssistantIcon.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import Icon02Agent from "../assets/icon-nova.svg";
-
-export const AIAssistantIcon = () => (
-  <img src={Icon02Agent} aria-hidden="true" alt="" width={18} height={18} style={{ width: 18, height: 18}} />
-);
